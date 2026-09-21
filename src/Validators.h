4334e47d3c8ff2c5e5fbcf2cce18748cc6d9dd3f
krism-eu <210770240+krism-eu@users.noreply.github.com>
2026-09-21 22:50:13 +0200

    Add shared input validators

---
+++ src/Validators.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <QString>
+
+namespace Validators {
+
+bool packageName(const QString &value);
+bool repositoryId(const QString &value);
+bool repositoryUrl(const QString &value);
+bool bootToken(const QString &value);
+bool grubEntry(const QString &value);
+bool containerName(const QString &value);
+bool flatpakId(const QString &value);
+
+}
